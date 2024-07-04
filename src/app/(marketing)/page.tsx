ebf4958f2a864a64ebf5c11c89b2aa70f86099bd
darlegui <arleguidaniel@gmail.com>
2024-07-05 06:00:05 +0900

    Added shadcn and customized marketing page.

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from '@/components/ui/button';
 import { PawPrint } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
+import logo from '../../../public/logo.svg';
+import Logo from '@/components/logo';
 
 export default function Home() {
   return (
@@ -10,16 +14,21 @@ export default function Home() {
         width={519}
         height={472}
       />
-      {/* <PawPrint width={400} height={400} /> */}
       <div className="">
+        {/* <PawPrint width={24} height={24} strokeWidth={2} /> */}
+        <Logo />
         <h1 className="text-5xl font-semibold my-6 max-w-[600px]">
           Manage your <span className="font-extrabold">pet daycare</span> with ease
         </h1>
-        <p>
-          Use PetSoft to easily keep track of pets under your care. Get lifetime access for $29.99 because we are not a
-          greedy company. :3
+        <p className="text-2xl font-medium max-w-[600px]">
+          Use PetSoft to easily keep track of pets under your care. Get lifetime access for only $29.99.
         </p>
-        <div className="mt-10"></div>
+        <div className="mt-10 space-x-2">
+          <Button className="bg-emerald-600">
+            <Link href="/signup">Get Started</Link>
+          </Button>
+          <Button variant="secondary">Log In</Button>
+        </div>
       </div>
     </main>
   );
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
-import logo from '../../public/logo.svg';
 import Link from 'next/link';
+import logo from '../../public/logo.svg';
 
 export default function Logo() {
   return (
     <div>
       <Link href="/">
-        <Image src={logo} alt="PetCare logo" />;
+        <Image src={logo} alt="PetCare logo" />
       </Link>
     </div>
   );
