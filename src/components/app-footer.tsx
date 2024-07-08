bcdc62ed24e007cc52a9b1cff77f35505375c1e1
darlegui <arleguidaniel@gmail.com>
2024-07-08 20:42:21 +0900

    App Footer

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function AppFooter() {
-  return <div>AppFooter</div>;
+  return (
+    <footer className="border-t border-black/5 py-5 mt-auto">
+      <small className="opacity-80">
+        &copy; {new Date().getFullYear()} PetCare App by DArlegui. All rights reserved.{' '}
+      </small>
+    </footer>
+  );
 }
