3b071877fd7ab49061180cb4146151c3a0006b35
darlegui <arleguidaniel@gmail.com>
2024-07-10 15:53:59 +0900

    Dashboard Grid Layout

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,5 +1,9 @@
-import Branding from '@/components/branding';
-import Stats from '@/components/stats';
+import Branding from '@/components/_dashboard/branding';
+import ContentBlock from '@/components/_dashboard/content-block';
+import PetDetails from '@/components/_dashboard/pet-details';
+import PetList from '@/components/_dashboard/pet-list';
+import SearchForm from '@/components/_dashboard/search-form';
+import Stats from '@/components/_dashboard/stats';
 
 export default function Dashboard() {
   return (
@@ -7,6 +11,23 @@ export default function Dashboard() {
       <div className="flex items-center justify-between text-white py-8">
         <Branding />
         <Stats />
+      </div>
+      <div className="grid grid-cols-3 grid-rows-[45px_1fr] gap-4 h-[600px]">
+        <div className="row-start-1 row-span-1 col-start-1 col-span1">
+          <SearchForm />
+        </div>
+
+        <div className="row-start-2 row-span-full col-start-1 col-span-1">
+          <ContentBlock>
+            <PetList />
+          </ContentBlock>
+        </div>
+
+        <div className="row-start-1 row-span-full col-start-2 col-span-full">
+          <ContentBlock>
+            <PetDetails />
+          </ContentBlock>
+        </div>
       </div>
     </main>
   );
---
+++ src/components/_dashboard/content-block.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ContentBlock({ children }: { children: React.ReactNode }) {
+  return <div className="bg-[#F7F8FA] shadow-sm rounded-sm overflow-hidden h-full w-full">{children}</div>;
+}
