e803e9db98c4d744b612720ef20c3b23b6498550
darlegui <arleguidaniel@gmail.com>
2024-07-17 16:58:26 +0900

    Update selected pet id and added selected pet styling.

--- src/contexts/pet-context-provider.tsx
+++ src/contexts/pet-context-provider.tsx
@@ -10,13 +10,20 @@ interface PetContextProviderProps {
 interface TPetContext {
   pets: PetProps[];
   selectedPetId: string | null;
+  handleChangeSelectedPetId: (id: string) => void;
 }
 
 export const PetContext = createContext<TPetContext | null>(null);
 
 export default function PetContextProvider({ data, children }: PetContextProviderProps) {
   const [pets, setPets] = useState(data);
-  const [selectedPetId, setSelectedPetId] = useState(null);
+  const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
 
-  return <PetContext.Provider value={{ pets, selectedPetId }}>{children}</PetContext.Provider>;
+  const handleChangeSelectedPetId = (id: string) => {
+    setSelectedPetId(id);
+  };
+
+  return (
+    <PetContext.Provider value={{ pets, selectedPetId, handleChangeSelectedPetId }}>{children}</PetContext.Provider>
+  );
 }
