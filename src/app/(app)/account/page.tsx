2c2721dd105115a1217e1293e437c2c7375871c7
darlegui <arleguidaniel@gmail.com>
2024-08-21 18:32:30 +0900

    refactor: Update Account page to use next-auth for user authentication

--- src/app/(app)/account/page.tsx
+++ src/app/(app)/account/page.tsx
@@ -1,13 +1,22 @@
 import ContentBlock from '@/components/_dashboard/content-block';
 import H1 from '@/components/_marketing/h1';
+import { auth } from '@/lib/auth';
+import { redirect } from 'next/navigation';
 
-export default function Account() {
+export default async function Account() {
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect('/login');
+  }
+
   return (
     <main>
       <H1 className="my-8 text-white">Your Account</H1>
 
-      <ContentBlock className="h-[500px] flex justify-center items-center">
-        <p className="text-lg">Logged in as...</p>
+      <ContentBlock className="h-[500px] flex flex-col justify-center items-center">
+        <p className="text-lg">Logged in as {session?.user?.email}</p>
+        <p className="text-lg">Session ends in {session?.expires}</p>
       </ContentBlock>
     </main>
   );
