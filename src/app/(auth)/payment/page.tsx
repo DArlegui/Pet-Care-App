6692538b326c8dcb7b2a84cb06374d1b50481fda
darlegui <arleguidaniel@gmail.com>
2024-09-16 00:06:57 -0700

    Refactor PaymentPage component to handle payment status and use useTransition hook

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -3,17 +3,32 @@
 import { createCheckoutSession } from '@/actions/actions';
 import H1 from '@/components/_marketing/h1';
 import { Button } from '@/components/ui/button';
+import { useTransition } from 'react';
 
-export default function PaymentPage() {
+export default function PaymentPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const [isPending, startTransition] = useTransition();
+
   return (
     <main className="flex flex-col items-center space-y-9">
-      <H1>PetSoft access requires payment</H1>
-      <Button
-        onClick={async () => {
-          await createCheckoutSession();
-        }}>
-        Buy lifetime access for $29.99
-      </Button>
+      <H1>PetCare access requires payment</H1>
+
+      {!searchParams.success && (
+        <Button
+          disabled={isPending}
+          onClick={async () => {
+            startTransition(() => {
+              createCheckoutSession();
+            });
+          }}>
+          Buy lifetime access for $29.99
+        </Button>
+      )}
+      {searchParams.success && <p className="tx-sm text-green-700">Payment successful!</p>}
+      {searchParams.cancelled && <p className="tx-sm text-red-700">Payment cancelled.</p>}
     </main>
   );
 }
